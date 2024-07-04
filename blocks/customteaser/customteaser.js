d29a53cbafcc52a86c560bf30522573b0d04b398
Tanaya Saha <tanaya.saha@nagarro.com>
2024-07-04 17:36:11 +0530

    push changes

--- blocks/customteaser/customteaser.js
+++ blocks/customteaser/customteaser.js
@@ -1,13 +1,28 @@
 export default function decorate(block) {
-    const [titleEl,pretitleEl] = block.children;
-    console.log('test',titleEl);
-    console.log('test',pretitleEl);
+    function getDemoTeaser(){
+    const [titleHeadingEl,titleDescriptionEl] = block.children;
+    console.log('test',titleHeadingEl);
+    console.log('test',titleDescriptionEl);
 
-    /*const customTeaser =
+
+    const titleHeading = titleHeadingEl?.textContent?.trim();
+    const titleDescription = titleDescriptionEl?.textContent?.trim();
+
+    return {
+        titleHeading,
+        titleDescription
+      };
+
+    }
+
+    const demoTeaser = getDemoTeaser(block);
+    
+
+    const customTeaser =
         `<div class="leftcolumn">
             <div class="card">
-                <h2>TITLE HEADING</h2>
-                <h5>Title description, Dec 7, 2017</h5>
+                 ${(demoTeaser.titleHeading) ? `<h2>${demoTeaser.titleHeading}</h2>` : ''}
+                 ${(demoTeaser.titleDescription) ? `<h5>${demoTeaser.titleDescription}</h5>` : ''}
                 <div class="fakeimg" style="height:200px;">Image</div>
                 <p>Some text..</p>
                 <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
@@ -18,5 +33,5 @@ block.innerHTML = `
 <div class="customTeaser__wrapper ">
     ${customTeaser}
 </div>
-`;*/
+`;
 }
