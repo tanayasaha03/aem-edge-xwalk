261836e6ed3566114f6b10e08fb1ac76d47e4a45
Tanaya Saha <tanaya.saha@nagarro.com>
2024-07-05 11:16:01 +0530

    push changes

--- blocks/demoMyTeaser/demoMyTeaser.js
+++ blocks/demoMyTeaser/demoMyTeaser.js
@@ -51,7 +51,7 @@ export default function decorate(block) {
                
                
                 <p>Some text..</p>
-                <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+                <p>Sunt in culpa qui test officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
             </div>
         </div>`;
 
